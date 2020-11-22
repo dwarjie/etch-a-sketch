321adb6cc37fe6037be12b65de337e452396197f
dwarjie <mozencode@gmail.com>
2020-11-22 23:12:44 +0800

    first version of etch-a-sketch

--- app.js
+++ app.js
@@ -1,24 +1,81 @@
 // Create a 16x16 grid using js inside of the container tag
 const gridContainer = document.querySelector('#container');
-const items = document.querySelectorAll('.div-items');
+const root = document.documentElement; // for the css
+const btnSmall = document.querySelector('#small');
+const btnMed = document.querySelector('#medium');
+const btnLarge = document.querySelector('#large');
+const btnClear = document.querySelector('#clear');
+let canvasSize = 16;
+
+// adding event click to buttons
+btnSmall.addEventListener('click', e => {
+	setCanvas(e);
+});
+btnMed.addEventListener('click', e => {
+	setCanvas(e);
+});
+btnLarge.addEventListener('click', e => {
+	setCanvas(e);
+});
+btnClear.addEventListener('click', clearCanvas);
+
+divLoops();
+// set the size of the canvas
+root.style.setProperty('--grid-rows', canvasSize + ', 2fr');
+root.style.setProperty('--grid-cols', canvasSize + ', 2fr');
+
+function divLoops() {
+	for(let i = 1; i <= canvasSize; i++) {
+	    // loop 16 times to make the column for row i
+	    for(let j = 1; j <= canvasSize; j++) {
+	        createDiv();
+	    }
+	}
+}
 
 function createDiv() {
     const divElem = document.createElement('div');
     divElem.classList.add('div-items');
 
     gridContainer.appendChild(divElem);
+    // add eventListener after div is created
+    divElem.addEventListener('mouseenter', e => {
+    	addColor(e);
+    });
 }
 
-// loop 16 times for the row
-for(let i = 1; i <= 16; i++) {
-    // loop 16 times to make the column for row i
-    for(let j = 1; j <= 16; j++) {
-        createDiv();
+function addColor(e) {
+	let divElem = e.target;
+    if (!(divElem.classList.contains('color_1'))) {
+    	divElem.classList.add('color_1');
     }
 }
 
-items.forEach((box) => box.addEventListener('onmouseenter', addColor));
+function removeAllChild(parent) {
+	while (parent.firstChild) {
+		parent.removeChild(parent.firstChild);
+	}
+} 
 
-function addColor(e) {
-    console.log(e.target);
+function setCanvas(e) {
+	let btnElem = e.target.id;
+	if (btnElem == 'small') {
+		canvasSize = 16;
+	} else if (btnElem == 'medium') {
+		canvasSize = 32;
+	} else {
+		canvasSize = 64;
+	}
+	removeAllChild(gridContainer);
+	root.style.setProperty('--grid-rows', canvasSize + ', 2fr');
+	root.style.setProperty('--grid-cols', canvasSize + ', 2fr');
+	console.log(canvasSize);
+	divLoops();
+}
+
+function clearCanvas() {
+	const items = document.querySelectorAll('.div-items');
+	for (const item of items) {
+		item.classList.remove('color_1');
+	}
 }
